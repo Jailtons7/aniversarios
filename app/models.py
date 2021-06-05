503e0e213c71ee731a3c23b4ac2e40462d3e653b
Jailton Silva <jss1@ifal.edu.br>
2021-06-05 20:39:30 -0300

    Criando o modelo de aniversários

--- app/models.py
+++ app/models.py
@@ -1,1 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+class Aniversarios(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    aniversariante = models.CharField(max_length=255)
+    dt_aniversario = models.DateField(verbose_name="Data de Aniversário")
+
+    class Meta:
+        verbose_name = "Aniversários"
+        verbose_name_plural = "Aniversários"
+
+    def __str__(self):
+        return f"{self.aniversariante}"
