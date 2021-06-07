0f76dd8ca071ecea7b6b6599281f658e90fe54ab
Jailton Silva <jss1@ifal.edu.br>
2021-06-07 10:42:50 -0300

    Formulário de aniversários

---
+++ app/forms.py
@@ -0,0 +1,13 @@
+from django.forms import ModelForm, TextInput, DateInput
+
+from app.models import Aniversarios
+
+
+class AniversariosForm(ModelForm):
+    class Meta:
+        model = Aniversarios
+        fields = ('aniversariante', 'dt_aniversario')
+        widgets = {
+            'aniversariante': TextInput(attrs={'class': 'form-control'}),
+            'dt_aniversario': DateInput(attrs={'class': 'form-control'})
+        }
