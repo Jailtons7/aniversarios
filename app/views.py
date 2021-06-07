ae43dee27c131b30acacbee169877ac375ec022c
Jailton Silva <jss1@ifal.edu.br>
2021-06-07 10:44:48 -0300

    Adicionando view de listagem dos aniversários

--- app/views.py
+++ app/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.views.generic import ListView, CreateView
 
-# Create your views here.
+from app.models import Aniversarios
+from app.forms import AniversariosForm
+
+
+class BirthdayCalendarView(ListView):
+    template_name = "aniversarios/base.html"
+    model = Aniversarios
+
+    def get_queryset(self):
+        return self.model.objects.all()
+
+    def get_context_data(self, **kwargs):
+        context = super(BirthdayCalendarView, self).get_context_data(**kwargs)
+        context['aniversario_form'] = AniversariosForm()
+        return context
