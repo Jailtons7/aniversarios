8a8d5e9cd34de5adbf6af13940455bb5446ff919
Jailton Silva <jss1@ifal.edu.br>
2021-06-08 08:15:12 -0300

    deixando dt_aniversario com o tipo date

--- app/forms.py
+++ app/forms.py
@@ -9,5 +9,5 @@ class AniversariosForm(ModelForm):
         fields = ('aniversariante', 'dt_aniversario')
         widgets = {
             'aniversariante': TextInput(attrs={'class': 'form-control'}),
-            'dt_aniversario': DateInput(attrs={'class': 'form-control'})
+            'dt_aniversario': DateInput(attrs={'class': 'form-control', 'type': 'date'})
         }
