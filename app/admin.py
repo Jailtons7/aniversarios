8ef362247a938b209c5a22be0a87dbe49df917e0
Jailton Silva <jss1@ifal.edu.br>
2021-08-14 23:17:51 -0300

    feat: registering custom model on django admin

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Aniversarios
+
+
+@admin.register(Aniversarios)
+class AniversariosAdmin(admin.ModelAdmin):
+    list_display = ('aniversariante', 'user', 'dt_aniversario')
+    list_display_links = ('aniversariante',)
+    search_fields = ('aniversariante', 'user')
+
