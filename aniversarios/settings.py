b53ba26e4f287f44314a249ebf9584d7b8b03915
Jailton Silva <jss1@ifal.edu.br>
2024-05-12 00:37:37 -0300

    feat: change login redirects

--- aniversarios/settings.py
+++ aniversarios/settings.py
@@ -133,3 +133,7 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'authentication.CustomUser'
+
+LOGIN_REDIRECT_URL = '/'
+
+LOGOUT_REDIRECT_URL = '/'
